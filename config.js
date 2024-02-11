a3ada3bc0648aa1ab7f58bc17daef4c17d70e94a
emtry <pyzlypz@gmail.com>
2024-02-11 23:04:23 +0800

    Initial commit

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ let config = {
     username: process.env.USERNAME || "", 
     password: process.env.PASSWORD || "", 
     process_workers: process.env.PROCESS_WORKERS || 2, 
-    loglevel: process.env.LOGLEVEL || 'debug', 
+    loglevel: process.env.LOGLEVEL || 'info', 
     proxy: process.env.PROXY || "", 
     timeout: process.env.TIMEOUT || 60000, 
-    headless: process.env.HEADLESS || false, 
+    headless: process.env.HEADLESS || "new", 
 };
 
 module.exports = config
